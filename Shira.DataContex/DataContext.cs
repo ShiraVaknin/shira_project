98adb87d767851815061c7149c841c482bd60ce5
student <student@541474mb.local>
2023-01-05 14:51:58 +0200

    DataContext2

---
+++ Shira.DataContex/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shira.Repositories.Entities;
+using Shira.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shira.DataContex
+{
+    public class DataContext : DbContext, IContext
+    {
+        
+    }
+}
